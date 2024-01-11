0a193a84845a83ad87f1a7b9a9b604c42a6eb8dc
RUDDYYB <155786758+RUDDYYB@users.noreply.github.com>
2024-01-10 21:21:14 -0400

    Add files via upload

---
+++ PRACTICA DE SUMA EN C++.cpp
@@ -0,0 +1,28 @@
+/*
+Titulo: primerprograma con comentarios
+Autor: Juan David Meza Gonzalez
+Descripcion:Primer programapara analizar su estructura.
+Fecha: 2024-01-10
+*/
+
+
+#include <iostream> //inclusiion de la libreria de entrada y salida
+using namespace std; // el uso del espacio el nombre para STD de la I/O
+int main () //Funcion pricipal para main para retornar un entero
+{
+	int numero; // declaracion de la variable como entero
+	numero = 5 + 5; // asignacon de la suma de 5+5 a la variable numero
+	/*impresion del resultado de la variable numero y adicion al final 
+	del salto de linea
+*/
+	
+	cout << " El resultado de la suma es:" << numero << endl;
+	return 0; //retorno de 0 para finalizar el programa
+}
+	
+	
+	
+	
+	
+	
+	
