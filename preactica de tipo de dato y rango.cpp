78878b81aad98c0f4b469c85fd7f5d35d02a7791
RUDDYYB <155786758+RUDDYYB@users.noreply.github.com>
2024-01-11 21:27:45 -0400

    Add files via upload

---
+++ preactica de tipo de dato y rango.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+	short int numero_entero_p;
+	unsigned short int numero_entero_p1 ;
+	int numero_entero;
+	float numero_decimal;
+	double numero_decimal_g;
+	//asignando valoreslimites de los tipos de datos
+	numero_entero_p=32767;//valor limite del dato
+	cout <<"numero entero pequeño :"<<numero_entero_p<<endl;
+	numero_entero_p=-32768;//valor fuera del rango
+	cout <<"numero entero pequeño :"<<numero_entero_p<<endl;
+}
