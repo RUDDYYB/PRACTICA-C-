8fff55d9d5a18be86ff9dd54e94222f6d158451a
RUDDYYB <155786758+RUDDYYB@users.noreply.github.com>
2024-01-12 21:59:22 -0400

    Add files via upload

---
+++ ejemplos de if.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	//solicitar un numero y verificar y si par o impar
+	//MOD en C++ SE UTILIZA %
+	int numero;
+	cout<<"ingrese numero"<< endl;
+	cin>>numero;
+	
+	if(numero %2 == 0)
+	{
+		cout << "es un numero par" << endl;
+	}
+	else
+	{
+		cout << "no es par" << endl;
+	}
+	return 0;
+}
+
