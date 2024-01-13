0e3b9fadb46c846dac66d5b0d0ae98c26361ebaa
RUDDYYB <155786758+RUDDYYB@users.noreply.github.com>
2024-01-12 21:59:11 -0400

    Add files via upload

---
+++ secuencia de if.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int resultado =0;
+	cout <<"cuanto es 10+5?:";
+	cin>>resultado;
+	if(resultado == (10+5))
+	{
+		cout<<"respuesta corecta. FELICITACIONES:"<<endl;
+	}
+	else
+	{
+		cout<<"Respuesta Incorrecta"<<endl;
+	}
+		
+		
+	return 0;
+}
+
